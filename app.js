986169f8c704d9d1899c77f7c0bbb31bf29dc200
Denis Ouma <53114809+DenisOuma@users.noreply.github.com>
2021-10-28 01:12:53 +0000

    Filtering of Tasks

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function loadEventListeners(){
   taskList.addEventListener('click', removeTask);
   // Clear All Task
   clearButton.addEventListener('click', clearTask);
+  // Filter Tasks
+  taskFilter.addEventListener('keyup', filterTask);
 }
 
 function addTask(e){
@@ -30,6 +32,8 @@ function addTask(e){
     li.appendChild(link);
     // Append li task to ul
     taskList.appendChild(li)
+
+    taskInput.value = "";
   }
 
   e.preventDefault()
@@ -47,18 +51,27 @@ function removeTask(e){
 // Clear all tasks function
 
 function clearTask(e){
-  
-    console.log(taskList)
-
-  if(taskList==undefined){
+  if(taskList.children.length === 0){
     console.log(taskList.nodeType)
     alert("your tas list is,Empty Please add some Tasks")
-  }else{
-    if(confirm("Are you sure you want to clear your task list")){
-    console.log(taskList)
-
-      taskList.remove()
-    }
+  }else if(taskList.children.length > 0){
+      taskList.remove
+      (confirm("Are you sure you want to clear your task list"))
   }
   e.preventDefault();
+}
+
+// Filter Task from the List
+function filterTask(e){
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      tas.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
+  e.preventDefault()
 }
